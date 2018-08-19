7de03e6b05739c8cf24dcb10ae99c9063b2ccd93
danmj <daniel.jung@stern.nyu.edu>
2018-08-19 15:29:35 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -17,18 +17,28 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coupons_applied = {}
-  coupons.each do |coupon|
-    if cart.key?(coupon[:item])
-      coupon_count = 0
-      until coupon[:num] > cart[coupon[:item]][:count]
-        cart[coupon[:item]][:count] -= coupon[:num]
-        cart["#{coupon[:item]} W/COUPON"] = {price: coupon[:cost], clearance: cart[coupon[:item]][:clearance], count: coupon_count += 1}
+  output = {}
+  cart.each do |key,value|
+    coupons.each do |coupon|
+      if (coupon[:item] == key && value[:count] >= coupon[:num])
+        new_key = key + " W/COUPON"
+        if output.has_key?(new_key)
+          output[new_key][:count] += 1
+          # output[key][:count] = output[key][:count] - coupon[:num]
+          value[:count]= value[:count] - coupon[:num]
+        else
+          output[new_key] = {}
+          output[new_key][:price] = coupon[:cost]
+          output[new_key][:clearance] = value[:clearance]
+          output[new_key][:count] = 1
+          # output[key][:count] = output[key][:count] - coupon[:num]
+          value[:count] = value[:count] - coupon[:num]
+        end
       end
     end
   end
-  cart.merge(coupons_applied)
-end
+  cart.merge(output)
+end	end
 
 def apply_clearance(cart)
   cart.each {|grocery,value| cart[grocery][:price] = (cart[grocery][:price] * 0.8).round(2) if cart[grocery][:clearance] }
