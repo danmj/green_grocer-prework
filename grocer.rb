0dd13b41ee880f61560e9cdefc8721ff19828bd6
danmj <daniel.jung@stern.nyu.edu>
2018-08-19 15:23:49 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,4 +1,4 @@
-def consolidate_cart(cart:[])
+def consolidate_cart(cart)
   consolidated_cart = {}
   cart.each { |grocery| grocery.each { |product,values| consolidated_cart[product] = {price: values[:price], clearance: values[:clearance], count: cart.count(grocery)} } }
   consolidated_cart
